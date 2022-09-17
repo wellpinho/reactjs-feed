b61b124f78caa33eab2f878517bceb363497cae5
wellpinho <wellpinho@outlook.com>
2022-09-17 10:40:32 -0300

    post hrml

--- src/components/post/index.jsx
+++ src/components/post/index.jsx
@@ -1,9 +1,38 @@
+import styles from "./post.module.scss";
+
 const Post = ({ author, description }) => {
   return (
-    <div>
-      <strong>{author}</strong>
-      <p>{description}</p>
-    </div>
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img src="https://avatars.githubusercontent.com/u/9344482?v=4" />
+
+          <div className={styles.authorInfo}>
+            <strong>Wellington Pinho</strong>
+            <span>Software Engineer</span>
+          </div>
+        </div>
+
+        <time title="17 de Setembro ás 10:32" dateTime="2022-09-17 10:32">
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋 </p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+          👉
+        </p>
+        <p>
+          <a href="#">jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="#">#novoprojeto #nlw #rocketseat</a>
+        </p>
+      </div>
+    </article>
   );
 };
 
